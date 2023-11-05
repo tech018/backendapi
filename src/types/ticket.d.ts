eb7b6e1c72597c5f0dee79fa22ea99ac20f5e472
Joseph De Guzman Aguilar <43537259+tech018@users.noreply.github.com>
2023-11-05 22:34:33 +0800

    new updates

--- src/types/ticket.d.ts
+++ src/types/ticket.d.ts
@@ -1,4 +1,12 @@
 interface Ticket {
   description: string;
   title: string;
+  clientId: number;
+  assignee: string;
+  assignee_email: string;
+  reporter: string;
+  reporter_email: string;
+  ticket_type: string;
+  ticket_severity: string;
+  project: string;
 }
